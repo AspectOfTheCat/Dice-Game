960d23ce524c07d1dce232b7474af0c230d5b0cf
MBH369 <67119058+MBH369@users.noreply.github.com>
2020-06-18 20:44:33 -0400

    Update DiceGame.py

--- DiceGame.py
+++ DiceGame.py
@@ -1,15 +1,23 @@
 roll = input("Roll a 6 sided die, and tell me the number: ")
 
-print("Your roll is " + str(roll))
+if roll > 6:
+  print("Uh..no..")
+  
+elif roll < 1:
+  print("You lost!")
+  
+else:
 
-print("Bot roll is..")
+    print("Your roll is " + str(roll))
 
-from random import seed
-from random import choice
-seed()
-#The seed can be anything, but for the output to be different, the seed has to be different!
-botroll = choice(range(1,6,1))
-print(botroll)
+    print("Bot roll is..")
+
+    from random import seed
+    from random import choice
+    seed()
+    #The seed can be anything, but for the output to be different, the seed has to be different!
+    botroll = choice(range(1,6,1))
+    print(botroll)
 
 
 
