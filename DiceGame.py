835d0346de5b11801065a4765043834b64872794
MBH369 <67119058+MBH369@users.noreply.github.com>
2020-06-18 20:31:41 -0400

    Update DiceGame.py

--- DiceGame.py
+++ DiceGame.py
@@ -7,7 +7,8 @@ print("Bot roll is..")
 from random import seed
 from random import choice
 seed(1)
-sequence = [i for i in range(20)]
+#The seed can be anything, but for the output to be different, the seed has to be different!
+sequence = [i for i in range(6)]
 for _ in range(1):
         selection = choice(sequence)
         print(sequence)
